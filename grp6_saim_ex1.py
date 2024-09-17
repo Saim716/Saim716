6a59078a82903a5be5a2e2d4ebcaa8d68056ce9b
Saim716 <saimhersheywayne_bscs@plmun.edu.ph>
2024-09-17 10:12:27 +0800

    grp6_saim_ex1.py

---
+++ grp6_saim_ex1.py
@@ -0,0 +1,35 @@
+#My first Phyton Script
+
+#Bscs3f
+#Exercise 1
+
+print ("Hello, Im Hershey Wain Saim BSCS 3F")
+
+age = 22
+name = "Hershey"
+gender = "Female"
+
+print("")
+
+print ("Name:",name)
+print ("Age:",age)
+print ("Gender:",gender)
+
+print("")
+mikee = "Mikee"
+lontoc = "Jhon Cedric"
+lucero = "Regan"
+melvin = "Melvin"
+
+print (mikee)
+print (lontoc)
+print (lucero)
+print (melvin)
+
+
+ 
+print("")
+ 
+age,edad,num = 25 ,28, 23
+
+print(age,edad,num)
